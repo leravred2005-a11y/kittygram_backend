c38b51d892119cfa20e494a768d2d130d229e902
Лерочка 😽 <lerocka@Mac.bbrouter>
2026-05-27 00:48:04 +0300

    Обновлён README, исправлены admin.py и settings.py, добавлены скриншоты

--- cats/admin.py
+++ cats/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Cat, Like, Favorite
 
-# Register your models here.
+@admin.register(Cat)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color', 'owner')
+    list_filter = ('color', 'owner')
+    search_fields = ('name',)
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'cat', 'created_at')
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'cat', 'created_at')
